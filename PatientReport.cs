d4accf1d721f0cf53cd1ed4095649d1e7c067ee9
Diego Garcia <diego.u.garcia@solca.med.ec>
2021-09-30 07:44:42 -0700

    Added printing functionality

--- PatientReport.cs
+++ PatientReport.cs
@@ -9,6 +9,7 @@ using VMS.TPS.Common.Model.API;
 using VMS.TPS.Common.Model.Types;
 using PatientReport.Views;
 using PatientReport.ViewModels;
+using Prism.Events;
 
 // TODO: Replace the following version attributes by creating AssemblyInfo.cs. You can do this in the properties of the Visual Studio project.
 [assembly: AssemblyVersion("1.0.0.1")]
@@ -30,11 +31,21 @@ namespace VMS.TPS
         public void Execute(ScriptContext context, System.Windows.Window window, ScriptEnvironment environment)
         {
             // TODO : Add here the code that is called when the script is launched from Eclipse.
+            // Eclipse cannot see oxyplot unless a dummy reference exists in this class.
+            OxyPlot.Wpf.AngleAxis dummy = new OxyPlot.Wpf.AngleAxis(); // for binary plugin it has to be here.
+            // DVH vms require EventAggregator.
+            IEventAggregator eventAggregator = new EventAggregator();
             window.Content = new MainView()
             {
-                DataContext = new MainViewModel(new PatientViewModel(context.Patient),
-                new PlanViewModel(context.PlanSetup),
-                new FieldViewModel(context.PlanSetup)
+                DataContext = new MainViewModel
+                (
+                    new PatientViewModel(context.Patient),
+                    new PlanViewModel(context.PlanSetup),
+                    new FieldViewModel(context.PlanSetup),
+                    new DVHPlot.ViewModels.DVHViewModel(context.PlanSetup, eventAggregator),
+                    new DVHPlot.ViewModels.DVHSelectionViewModel(context.PlanSetup, eventAggregator),
+                    context.Patient,
+                    context.CurrentUser
                 )
             };
         }
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,24 +1,98 @@
-﻿using System;
+﻿using DVHPlot.ViewModels;
+using Microsoft.Win32;
+using OxyPlot.Wpf;
+using PatientReport.Views;
+using PDFtoAria;
+using Prism.Commands;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Media.Imaging;
+using VMS.TPS.Common.Model.API;
 
 namespace PatientReport.ViewModels
 {
     public class MainViewModel
     {
+   
         public MainViewModel(PatientViewModel patientViewModel, 
             PlanViewModel planViewModel, 
-            FieldViewModel fieldViewModel)
+            FieldViewModel fieldViewModel,
+            DVHViewModel dVHViewModel,
+            DVHSelectionViewModel dVHSelectionViewModel,
+            Patient patient,
+            User user
+            )
         {
+            _patient = patient;
+            _user = user;
             PatientViewModel = patientViewModel;
             PlanViewModel = planViewModel;
             FieldViewModel = fieldViewModel;
+            DVHViewModel = dVHViewModel;
+            DVHSelectionViewModel = dVHSelectionViewModel;
+
+            PrintCommand = new DelegateCommand(OnPrint);
+            AriaPostCommand = new DelegateCommand(OnPostToAria);
+        }
+
+        private void OnPostToAria()
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "PDF (*.pdf)|*.pdf";
+            if (file.ShowDialog() == true)
+            {
+                var BinaryContent = File.ReadAllBytes(file.FileName);
+                // change "Consent" to the correct type
+                CustomInsertDocumentsParameter.PostDocumentData(_patient.Id, _user,
+                    BinaryContent, "Plan Report",
+                    new VMS.OIS.ARIALocal.WebServices.Document.Contracts.DocumentType { DocumentTypeDescription = "Consent" });
+            }
+        }
+
+        private void OnPrint()
+        {
+            FlowDocument fd = new FlowDocument { FontSize = 12, FontFamily = new System.Windows.Media.FontFamily("Franklin Gothic") };
+            fd.Blocks.Add(new Paragraph(new Run("Treatment Plan Report")));
+            fd.Blocks.Add(new BlockUIContainer(new PatientView { DataContext = PatientViewModel.PatientInfo }));
+            fd.Blocks.Add(new BlockUIContainer(new PlanView { DataContext = PlanViewModel.PlanInfo }));
+            foreach (var field in FieldViewModel.Fields)
+            {
+                fd.Blocks.Add(new BlockUIContainer(new FieldDetailsView { DataContext = field }));
+            }
+            BitmapSource bmp = new PngExporter().ExportToBitmap(DVHViewModel.DVHPlotModel);
+            fd.Blocks.Add(new BlockUIContainer(new System.Windows.Controls.Image
+            {
+                Source = bmp,
+                Height = 700,
+                Width = 900
+            }));
+            System.Windows.Controls.PrintDialog printer = new System.Windows.Controls.PrintDialog();
+            printer.PrintTicket.PageOrientation = System.Printing.PageOrientation.Landscape;
+            fd.PageHeight = 816;
+            fd.PageWidth = 1056;
+            fd.PagePadding = new System.Windows.Thickness(50);
+            fd.ColumnGap = 0;
+            fd.ColumnWidth = 1056;
+            IDocumentPaginatorSource source = fd;
+            if (printer.ShowDialog() == true) // microsoft print2pdf
+            {
+                printer.PrintDocument(source.DocumentPaginator, "TreatmentPlanReport");
+            }
         }
 
         public PatientViewModel PatientViewModel { get; }
         public PlanViewModel PlanViewModel { get; }
         public FieldViewModel FieldViewModel { get; }
+        public DVHSelectionViewModel DVHSelectionViewModel { get; }
+        public DelegateCommand PrintCommand { get; private set; }
+        public DelegateCommand AriaPostCommand { get; private set; }
+        public DVHViewModel DVHViewModel { get; }
+        public Patient _patient { get; private set; }
+        public User _user { get; private set; }
     }
 }
